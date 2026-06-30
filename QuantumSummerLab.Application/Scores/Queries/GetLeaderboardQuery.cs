6dbe0d2ed3adc515a872c78a67c833f13c06ff6d
Djohnnie <7031043+Djohnnie@users.noreply.github.com>
2026-06-30 23:15:49 +0200

    Excluded admins from leaderboard

--- QuantumSummerLab.Application/Scores/Queries/GetLeaderboardQuery.cs
+++ QuantumSummerLab.Application/Scores/Queries/GetLeaderboardQuery.cs
@@ -41,10 +41,10 @@ public class GetLeaderboardQueryHandler : IRequestHandler<GetLeaderboardQuery, G
     {
         using var dbContext = _scopeFactory.CreateScope().ServiceProvider.GetRequiredService<QuantumSummerLabDbContext>();
 
-        var teams = await dbContext.Teams.Where(x => !x.IsArchived).ToListAsync(cancellationToken);
+        var teams = await dbContext.Teams.Where(x => !x.IsArchived && !x.IsAdmin).ToListAsync(cancellationToken);
 
         var scores = await dbContext.Scores
-            .Where(x => !x.Team.IsArchived)
+            .Where(x => !x.Team.IsArchived && !x.Team.IsAdmin)
             .Include(s => s.Team)
             .GroupBy(s => s.Team)
             .Select(g => new
